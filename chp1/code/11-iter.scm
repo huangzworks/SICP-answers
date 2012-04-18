1015584ed668522eb0c02fbe2eeeb4d2d08c1383
huangz1990 <huangz1990@gmail.com>
2012-04-18 09:51:18 +0800

    添加练习 11

---
+++ chp1/code/11-iter.scm
@@ -0,0 +1,13 @@
+;;; 11-iter.scm
+
+(define (f n)
+    (f-iter 2 1 0 0 n))
+
+(define (f-iter a b c i n)
+    (if (= i n)
+        c
+        (f-iter (+ a (* 2 b) (* 3 c))   ; new a
+                a                       ; new b
+                b                       ; new c
+                (+ i 1)
+                n)))
