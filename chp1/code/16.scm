cb7b6fe1c77aa4b718314b38c554b9712c8aac2e
huangz1990 <huangz1990@gmail.com>
2012-04-18 10:24:27 +0800

    添加练习 16

---
+++ chp1/code/16.scm
@@ -0,0 +1,16 @@
+;;; 16.scm
+
+(define (expt b n)
+    (expt-iter b n 1))
+
+(define (expt-iter b n a)
+    (cond ((= n 0)
+            a)
+          ((even? n)
+            (expt-iter (square b)
+                       (/ n 2)
+                       a))
+          ((odd? n)
+            (expt-iter b
+                       (- n 1)
+                       (* b a)))))
