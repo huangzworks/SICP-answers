f199524f93889fafd69e6a66984511dd1a4f60f6
huangz1990 <huangz1990@gmail.com>
2012-04-19 10:46:01 +0800

    添加练习 1.21

---
+++ chp1/code/p33-smallest-divisor.scm
@@ -0,0 +1,15 @@
+;;; p33-smallest-divisor.scm
+
+(define (smallest-divisor n)
+    (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n)
+            n)
+          ((divides? test-divisor n)
+            test-divisor)
+          (else
+            (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+    (= (remainder b a) 0))
