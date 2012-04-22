1ccec13510acbc8d4e2969ecd03d9ffb60a324b0
huangz1990 <huangz1990@gmail.com>
2012-04-22 14:10:40 +0800

    更新 28-fringe.scm 注释

--- chp2/code/28-fringe.scm
+++ chp2/code/28-fringe.scm
@@ -1,9 +1,9 @@
 ;;; 28-fringe.scm
 
 (define (fringe tree)
-    (cond ((null? tree) ; 空树
+    (cond ((null? tree)                         ; 空树
             '())
-          ((not (pair? tree))   ; 叶子
+          ((not (pair? tree))                   ; 叶子
             (list tree))
           (else
             (append (fringe (car tree))         ; 累积左子树所有元素
