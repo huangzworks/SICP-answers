03aa4863339d3c3fb13bc0e88ae96c9a8039fc7d
huangz1990 <huangz1990@gmail.com>
2012-04-23 12:23:22 +0800

    添加练习 2.38

---
+++ chp2/code/38-fold-left.scm
@@ -0,0 +1,9 @@
+;;; 38-fold-left.scm
+
+(define (fold-left op initial sequence)
+    (define (iter result rest)
+        (if (null? rest)
+            result
+            (iter (op result (car rest))
+                  (cdr rest))))
+    (iter initial sequence))
---
+++ chp2/code/38-fold-right.scm
@@ -0,0 +1,5 @@
+;;; 38-fold-right.scm
+
+(load "p78-accumulate.scm")
+
+(define fold-right accumulate)
