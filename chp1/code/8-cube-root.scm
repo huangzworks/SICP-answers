b971ef42e6cd16a5c7e0baba6d7daa693e25a2ef
huangz1990 <huangz1990@gmail.com>
2012-04-26 21:48:28 +0800

    修正练习 1.8

--- chp1/code/8-cube-root.scm
+++ chp1/code/8-cube-root.scm
@@ -2,23 +2,23 @@
 
 ;; 求立方
 
-(define (cube x)                                ; + 新增加或被修改过的代码都用 + 号标示
-    (* x (square x)))                           ; +
+(define (cube x)
+    (* x (square x)))
 
 ;; 求次方根
 
-(define (cube-root x)                           ; +
-    (cube-root-iter 1.0 x))                     ; +
+(define (cube-root x)
+    (cube-root-iter 1.0 x))
 
-(define (cube-root-iter guess x)                ; +
+(define (cube-root-iter guess x)
     (if (good-enough? guess x)
         guess
-        (cube-root-iter (improve guess x)       ; +
+        (cube-root-iter (improve guess x)
                         x)))
 
 (define (good-enough? guess x)
     (< (abs (- (cube guess) x)) 0.001))
 
-(define (improve guess x)                       ; +
-    (/ (+ (/ x (square guess)) (* 2 guess))     ; +
-       3))                                      ; +
+(define (improve guess x)
+    (/ (+ (/ x (square guess)) (* 2 guess))
+       3))
