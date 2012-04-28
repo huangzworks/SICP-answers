974fc9911be844d621a4363e484e9c27d4e8aeea
huangz1990 <huangz1990@gmail.com>
2012-04-28 13:25:44 +0800

    添加练习 1.35

---
+++ chp1/code/35-golden-ratio.scm
@@ -0,0 +1,7 @@
+;;; 35-golden-ratio.scm
+
+(load "p46-fixed-point.scm")
+
+(define golden-ratio
+    (fixed-point (lambda (x) (+ 1 (/ 1 x)))
+                 1.0))
---
+++ chp1/code/p46-fixed-point.scm
@@ -0,0 +1,13 @@
+;;; p46-fixed-point.scm
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+    (define (close-enough? v1 v2)
+        (< (abs (- v1 v2)) tolerance))
+    (define (try guess)
+        (let ((next (f guess)))
+            (if (close-enough? guess next)
+                next
+                (try next))))
+    (try first-guess))
