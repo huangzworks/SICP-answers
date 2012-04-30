0cbfb1cb7c368719b5617621d3eaeba437481fc3
huangz1990 <huangz1990@gmail.com>
2012-05-01 01:13:36 +0800

    添加练习 2.18

---
+++ chp2/code/18-reverse.scm
@@ -0,0 +1,10 @@
+;;; 18-reverse.scm
+
+(define (reverse lst)
+    (iter lst '()))
+
+(define (iter remained-items result)
+    (if (null? remained-items)
+        result
+        (iter (cdr remained-items)
+              (cons (car remained-items) result))))
