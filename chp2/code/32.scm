46a9e6cee1b33f5c5161b0f47cf2b1905a43c319
huangz1990 <huangz1990@gmail.com>
2012-05-01 14:12:22 +0800

    添加练习 2.32

---
+++ chp2/code/32.scm
@@ -0,0 +1,9 @@
+;;; 32.scm
+
+(define (subsets s)
+    (if (null? s)
+        (list '())
+        (let ((rest (subsets (cdr s))))
+            (append rest (map (lambda (x)
+                                (cons (car s) x))
+                              rest)))))
