b7b8aaaa424f107421365f72d1e7ab1daf011f1f
huangz1990 <huangz1990@gmail.com>
2012-05-02 11:10:46 +0800

    添加练习 2.60

---
+++ chp2/code/60-adjoin-set.scm
@@ -0,0 +1,4 @@
+;;; 60-adjoin-set.scm
+
+(define (adjoin-set x set)
+    (cons x set))
--- chp2/code/p103-element-of-set.scm
+++ chp2/code/p103-element-of-set.scm
@@ -1,6 +1,9 @@
 ;;; p103-element-of-set.scm
 
 (define (element-of-set? x set)
-    (cond ((null? set) #f)
-          ((equal? x (car set)) #t)
-          (else (element-of-set? x (cdr set)))))
+    (cond ((null? set)
+            #f)
+          ((equal? x (car set))
+            #t)
+          (else
+            (element-of-set? x (cdr set)))))
