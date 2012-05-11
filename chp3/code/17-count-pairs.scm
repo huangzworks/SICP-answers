cce64889cf8d5fefce77a932b5c5b8ccf15e0300
huangz1990 <huangz1990@gmail.com>
2012-05-11 14:48:36 +0800

    添加 3.17

---
+++ chp3/code/17-count-pairs.scm
@@ -0,0 +1,12 @@
+;;; 17-count-pairs.scm
+
+(define (count-pairs x)
+    (length (inner x '())))
+
+(define (inner x memo-list)
+    (if (and (pair? x)
+             (false? (memq x memo-list)))
+        (inner (car x)
+               (inner (cdr x)
+                      (cons x memo-list)))
+        memo-list))
