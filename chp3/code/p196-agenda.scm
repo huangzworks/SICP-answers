fff641335346c90d75616fb5c1996028e6928d99
huangz1990 <huangz1990@gmail.com>
2012-05-15 21:31:23 +0800

    添加 3.31

--- chp3/code/p196-agenda.scm
+++ chp3/code/p196-agenda.scm
@@ -2,20 +2,32 @@
 
 (load "p181-queue.scm")
 
+; segment
+
 (define (make-time-segment time queue)
   (cons time queue))
+
 (define (segment-time s) (car s))
+
 (define (segment-queue s) (cdr s))
 
+; agenda
+
 (define (make-agenda) (list 0))
+
 (define (current-time agenda) (car agenda))
+
 (define (set-current-time! agenda time)
   (set-car! agenda time))
-  (define (segments agenda) (cdr agenda))
-  (define (set-segments! agenda segments)
+
+(define (segments agenda) (cdr agenda))
+
+(define (set-segments! agenda segments)
     (set-cdr! agenda segments))
-    (define (first-segment agenda) (car (segments agenda)))
-    (define (rest-segments agenda) (cdr (segments agenda)))
+
+(define (first-segment agenda) (car (segments agenda)))
+
+(define (rest-segments agenda) (cdr (segments agenda)))
 
 (define (empty-agenda? agenda)
   (null? (segments agenda)))
