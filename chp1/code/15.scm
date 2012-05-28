b688c88df3eefdc0b7dfcfb52e544e22c2d06369
huangz1990 <huangz1990@gmail.com>
2012-05-28 10:12:43 +0800

    添加 1.15

---
+++ chp1/code/15.scm
@@ -0,0 +1,10 @@
+;;; 15.scm
+
+(define (cube x) (* x x x))
+
+(define (p x) (- (* 3 x) (* 4 (cube x))))
+
+(define (sine angle)
+    (if (not (> (abs angle) 0.1))
+        angle
+        (p (sine (/ angle 3.0)))))
