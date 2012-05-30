16abae3f96c4caead0f15a1abc2338e55636e031
huangz1990 <huangz1990@gmail.com>
2012-05-30 15:16:39 +0800

    重构 2.21

---
+++ chp2/code/test-21-square-list-using-map.scm
@@ -0,0 +1,17 @@
+(load "test-manager/load.scm")
+(load "21-square-list-using-map")
+
+(define-each-check
+
+    (equal? (square-list '())
+            '())
+
+    (equal? (square-list (list 1 2 3 4))
+            (list (square 1)
+                  (square 2)
+                  (square 3)
+                  (square 4)))
+
+)
+
+(run-registered-tests)
