d99913d7a0516e2814d762ec19140288b264189e
huangz1990 <huangz1990@gmail.com>
2012-05-31 00:48:02 +0800

    重构 2.27

---
+++ chp2/code/test-27-deep-reverse.scm
@@ -0,0 +1,16 @@
+(load "test-manager/load.scm")
+(load "27-deep-reverse.scm")
+
+(define x (list (list 1 2) (list 3 4)))
+
+(define-each-check
+
+    (equal? (deep-reverse x)
+            (list (list 4 3) (list 2 1)))
+
+    (equal? (deep-reverse '())
+            '())
+
+)
+
+(run-registered-tests)
