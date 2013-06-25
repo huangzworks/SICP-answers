9c66040a8b1558c016ee11c1437908c4d9f295bd
huangz1990 <huangz1990@gmail.com>
2013-06-25 22:59:57 +0800

    修正一处代码注释错误，感谢 ljf007 的提醒。

--- chp2/code/31-tree-map-using-map.scm
+++ chp2/code/31-tree-map-using-map.scm
@@ -3,6 +3,6 @@
 (define (tree-map f tree)
     (map (lambda (sub-tree)
              (if (pair? sub-tree)
-                 (tree-map f sub-tree)  ; 递归处理左右分支
-                 (f sub-tree)))
+                 (tree-map f sub-tree)  ; 处理子树
+                 (f sub-tree)))         ; 处理节点
          tree))
