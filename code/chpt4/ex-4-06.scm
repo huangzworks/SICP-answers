e6ad2dcd1ea51f112362970b9cc55f59fe523421
ZHHY <baptistezh@126.com>
2013-08-15 23:07:56 +0200

    ex-4.7 code added

--- code/chpt4/ex-4-06.scm
+++ code/chpt4/ex-4-06.scm
@@ -7,20 +7,20 @@
 
 ;; Exercise 4.6
 ;; Let expressions are derived expressions, because
-;
-;  (let ((<var1> <exp1>) ... (<varn> <expn>))
-;    <body>)
-;
-;  is equivalent to
-;
-;  ((lambda (<var1> ... <varn>)
-;     <body>)
-;   <exp1>
-;   <expn>)
-;
+;;
+;; (let ((<var1> <exp1>) ... (<varn> <expn>))
+;;   <body>)
+;;
+;; is equivalent to
+;;
+;; ((lambda (<var1> ... <varn>)
+;;    <body>)
+;;  <exp1>
+;;  <expn>)
+;;
 ;; Implement a syntactic transformation let->combination that reduces
-;  evaluating let expressions to evaluating combinations of the type shown
-;  above, and add the appropriate clause to eval to handle let expressions.
+;; evaluating let expressions to evaluating combinations of the type shown
+;; above, and add the appropriate clause to eval to handle let expressions.
 
 (define (eval exp env)
   (cond ((self-evaluating? exp) exp)
