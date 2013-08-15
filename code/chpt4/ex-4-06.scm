4dfb38dfec1bee19e754c89e21c0cf1a898943e8
ZHHY <baptistezh@126.com>
2013-08-15 21:47:25 +0200

    ex-4.6 added

--- code/chpt4/ex-4-06.scm
+++ code/chpt4/ex-4-06.scm
@@ -15,9 +15,45 @@
 ;
 ;  ((lambda (<var1> ... <varn>)
 ;     <body>)
-;   <exp1> 
+;   <exp1>
 ;   <expn>)
 ;
 ;; Implement a syntactic transformation let->combination that reduces
 ;  evaluating let expressions to evaluating combinations of the type shown
 ;  above, and add the appropriate clause to eval to handle let expressions.
+
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((let? exp) (eval (let->combination exp) env))
+        ((lambda? exp)
+         (make-procedure (lambda-parameters exp)
+                         (lambda-body exp)
+                         env))
+        ((begin? exp)
+         (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (eval (cond->if exp) env))
+        ((application? exp)
+         (new-apply (eval (operator exp) env)       ;;'new-apply' see ATTENTION
+                (list-of-values (operands exp) env)))
+        (else
+          (error "Unknown expression type -- EVAL" exp))))
+
+(define (let? exp) (tagged-list? exp 'let))
+
+(define (let-body exp) (cddr exp))
+
+(define (let-parameters exp)
+  (map car (cadr exp)))
+
+(define (let-arguments exp)
+  (map cadr (cadr exp)))
+
+(define (let->combination exp)
+  (cons (make-lambda (let-parameters exp)
+                     (let-body exp))
+        (let-arguments exp)))
---
+++ code/chpt4/test-ex-4-06.scm
@@ -0,0 +1,36 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;                    Author : ZhHy
+;;                    Date   : 2010-08-11
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Test Exercise 4.6
+
+;; load all the original necessary procedures in the text book
+
+(load "chpt-4-1.scm")
+
+;; load modified EVAL procedure and let expression handles
+
+(load "ex-4-06.scm")
+
+;; define the global environment
+
+(define the-global-environment (setup-environment))
+
+;; test the following let expression
+;; (let ((a 1) (b 2)) (cons a b))
+
+(display ";;; ******************** TEST ****************************************")
+(newline)
+(display ";;; TYPE :")
+(newline)
+(display ";;; (let ((a 1) (b 2)) (cons a b))")
+(newline)
+(display ";;; you should see : (1 . 2)")
+(newline)
+(display ";;; ******************** TEST ****************************************")
+
+;; run the mimic interpretator
+(driver-loop)
