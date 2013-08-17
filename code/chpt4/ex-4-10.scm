8080d37084b2a10cdba24326d3c04b47a10d416f
ZHHY <baptistezh@126.com>
2013-08-18 00:20:35 +0200

    ex 4.9 code added, fix a prob in previous exercise
    The problem in ex 4.6 4.7 4.8 4.9 is tha the let paramter list could be
    null, and this is needed for dreving the do expression into a
    combination in which the first expression is a let expression without
    parameter
    
    I don't really understand the git remote and local, why I need to pull
    the remote branch when I make a amend locally

--- code/chpt4/ex-4-10.scm
+++ code/chpt4/ex-4-10.scm
@@ -6,7 +6,10 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Exercise 4.10
+;;
 ;; By using data abstraction, we were able to write an eval procedure that is
-;  independent of the particular syntax of the language to be evaluated.
-;  To illustrate this, design and implement a new syntax for Scheme by
-;   modifying the procedures in this section, without changing eval or apply.
+;; independent of the particular syntax of the language to be evaluated.
+;; To illustrate this, design and implement a new syntax for Scheme by
+;;  modifying the procedures in this section, without changing eval or apply.
+;;
+;; =============================================================================
