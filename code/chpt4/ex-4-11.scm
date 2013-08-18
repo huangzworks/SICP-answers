8952a5b99b2b6d1dd4ef45674d21f806618d456b
ZHHY <baptistezh@126.com>
2013-08-18 12:34:24 +0200

    prepared for ex 4.11 4.12 4.13

---
+++ code/chpt4/ex-4-11.scm
@@ -0,0 +1,15 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;                    Author : ZhHy
+;;                    Date   : 2010-08-18
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Exercise 4.11
+;;
+;; Instead of representing a frame as a pair of lists, we can
+;; represent a frame as a list of bindings, where each binding is a
+;; name-value pair. Rewrite the environment operations to use this
+;; alternative representation.
+;;
+;; =============================================================================
---
+++ code/chpt4/ex-4-12.scm
@@ -0,0 +1,16 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;                    Author : ZhHy
+;;                    Date   : 2010-08-18
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Exercise 4.12
+;;
+;; The procedures set-variable-value!, define-variable!,
+;; and lookup-variable-value can be expressed in terms of more
+;; abstract procedures for traversing the environment
+;; structure. Define abstractions that capture the common patterns and
+;; redefine the three procedures in terms of these abstractions.
+;;
+;; =============================================================================
