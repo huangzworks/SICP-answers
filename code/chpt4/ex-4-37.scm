0b4df75df7841757f1f83e4d4cdeb6881fed2f72
ZHHY <baptistezh@126.com>
2013-09-02 12:44:26 +0200

    prepared for chpt-4-3-2

---
+++ code/chpt4/ex-4-37.scm
@@ -0,0 +1,28 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;                    Author : ZhHy
+;;                    Date   : 2010-09-02
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Exercise 4.37
+;;
+;; Ben Bitdiddle claims that the following method for generating
+;; Pythagorean triples is more efficient than the one in exercise
+;; 4.35. Is he correct? (Hint: Consider the number of possibilities
+;; that must be explored.)
+;;
+;; (define (a-pythagorean-triple-between low high)
+;;   (let ((i (an-integer-between low high))
+;;         (hsq (* high high)))
+;;     (let ((j (an-integer-between i high)))
+;;       (let ((ksq (+ (* i i) (* j j))))
+;;         (require (>= hsq ksq))
+;;         (let ((k (sqrt ksq)))
+;;           (require (integer? k))
+;;           (list i j k))))))
+;;
+;; =============================================================================
+
+
+;; ANSWER:
