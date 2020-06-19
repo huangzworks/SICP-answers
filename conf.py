a44b61355d3d6ed5310b478f20124b0604c77ef6
xiaochi <cumt.xiaochi@gmail.com>
2020-06-19 22:59:35 +0800

    Revert "Update conf.py"
    This reverts commit bc9cef044b8962ab05fe4ba3342869157fb3433f.

--- conf.py
+++ conf.py
@@ -26,7 +26,6 @@ import sys, os
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.mathjax']
-mathjax_path = 'https://cdn.rawgit.com/mathjax/MathJax/2.7.1/MathJax.js'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
